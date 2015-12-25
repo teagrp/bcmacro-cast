9bf45a0d979c32ade2495d01ac00ca02318d6e0e
nakamura shinya <nakamura@pl.info.kochi-tech.ac.jp>
2015-12-25 17:22:37 +0900

    add samplecode

--- samplecode/jobject.cpp
+++ samplecode/jobject.cpp
@@ -1,16 +1,3 @@
-#include<iostream>
-
-template <typename T_n>
-T_n Add(T_n tnA, T_n tnB) {
-  return tnA + tnB;
-}
-
-template <typename>
-class nakamura;
-
-template <>
-class nakamura<void>;
-
 typedef char* jobject;
 
 template <typename Ty>
@@ -22,24 +9,14 @@ class C {
   }
 };
 
-int main() {
-  std::cout << "こんにちは、C++の世界へようこそ！" << std::endl;
-
-  double dValue = Add(1.3, 2.4);
-  int iValue = Add(1, 2);
-  
+int main() {  
   return 0;
 }
 
 
 class C<float> obj;
 class C<double> obj2;
-
-void f() {}
-
 class C<long long> obj3;
-
-
 typedef C<char> C_char;
 C_char obj4;
 
