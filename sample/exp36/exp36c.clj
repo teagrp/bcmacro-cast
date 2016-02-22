415913a009e2bb3e99e70469fb1c73f250c3a404
ayato1995 <ayato.k070206@outlook.jp>
2016-02-23 06:47:58 +0900

    インデントなどを修正

--- sample/exp36/exp36c.clj
+++ sample/exp36/exp36c.clj
@@ -1,129 +1,128 @@
-(do
-  (defrule exp361 @{:type [{:Pointee {:kind "Char-type"}} ... {:Pointee {:kind "Int-type"}}] :loc-begin loc}
-    `("Very bad1!!!" ~loc))
-  
-  (defrule exp362 @{:type [... {:Pointee {:kind "Void-type"}}] :loc-begin loc}
-    `("Very bad2!!!" ~loc))
+(defrule exp36-1 @{:type [{:Pointee {:kind "Char-type"}} ... {:Pointee {:kind "Int-type"}}]}
+ "Very bad1!!!")
 
-  (defrule exp363 @{:type [{:Pointee {:kind "Char-type"}} ... {:Pointee {:kind "Short-type"}}] :loc-begin loc}
-    `("Very bad3!!!" ~loc))
-  
-  (defrule exp364 @{:type [{:Pointee {:kind "Char-type"}} ... {:Pointee {:kind "UnsignedShot-type"}}] :loc-begin loc}
-    `("Very bad4!!!" ~loc))
+(defrule exp36-2 @{:type [... {:Pointee {:kind "Void-type"}}]}
+ "Very bad2!!!")
 
-  (defrule exp365 @{:type [{:Pointee {:kind "Char-type"}} ... {:Pointee {:kind "Long-type"}}] :loc-begin loc}
-    `("Very bad5!!!" ~loc))
+(defrule exp36-3 @{:type [{:Pointee {:kind "Char-type"}} ... {:Pointee {:kind "Short-type"}}]}
+ "Very bad3!!!")
 
-  (defrule exp366 @{:type [{:Pointee {:kind "Char-type"}} ... {:Pointee {:kind "Float-type"}}] :loc-begin loc}
-    `("Very bad6!!!" ~loc))
+(defrule exp36-4 @{:type [{:Pointee {:kind "Char-type"}} ... {:Pointee {:kind "UnsignedShot-type"}}]}
+ "Very bad4!!!")
 
-  (defrule exp367 @{:type [{:Pointee {:kind "Char-type"}} ... {:Pointee {:kind "UnsignedInt-type"}}] :loc-begin loc}
-    `("Very bad7!!!" ~loc))
+(defrule exp36-5 @{:type [{:Pointee {:kind "Char-type"}} ... {:Pointee {:kind "Long-type"}}]}
+ "Very bad5!!!")
 
-  (defrule exp368 @{:type [{:Pointee {:kind "Char-type"}} ... {:Pointee {:kind "UnsignedLong-type"}}] :loc-begin loc}
-    `("Very bad8!!!" ~loc))
+(defrule exp36-6 @{:type [{:Pointee {:kind "Char-type"}} ... {:Pointee {:kind "Float-type"}}]}
+ "Very bad6!!!")
 
-  (defrule exp369 @{:type [{:Pointee {:kind "Char-type"}} ... {:Pointee {:kind "Double-type"}}] :loc-begin loc}
-    `("Very bad9!!!" ~loc))
+(defrule exp36-7 @{:type [{:Pointee {:kind "Char-type"}} ... {:Pointee {:kind "UnsignedInt-type"}}]}
+ "Very bad7!!!")
 
-  (defrule exp3610 @{:type [{:Pointee {:kind "Char-type"}} ... {:Pointee {:kind "LongDouble-type"}}] :loc-begin loc}
-    `("Very bad10!!!" ~loc))
+(defrule exp36-8 @{:type [{:Pointee {:kind "Char-type"}} ... {:Pointee {:kind "UnsignedLong-type"}}]}
+ "Very bad8!!!")
 
-  (defrule exp3611 @{:type [{:Pointee {:kind "UnsignedChar-type"}} ... {:Pointee {:kind "Short-type"}}] :loc-begin loc}
-    `("Very bad11!!!" ~loc))
-  
-  (defrule exp3612 @{:type [{:Pointee {:kind "UnsignedChar-type"}} ... {:Pointee {:kind "UnsignedShot-type"}}] :loc-begin loc}
-    `("Very bad12!!!" ~loc))
+(defrule exp36-9 @{:type [{:Pointee {:kind "Char-type"}} ... {:Pointee {:kind "Double-type"}}]}
+ "Very bad9!!!")
 
-  (defrule exp3613 @{:type [{:Pointee {:kind "UnsignedChar-type"}} ... {:Pointee {:kind "Int-type"}}] :loc-begin loc}
-    `("Very bad13!!!" ~loc))
-  
-  (defrule exp3614 @{:type [{:Pointee {:kind "UnsignedChar-type"}} ... {:Pointee {:kind "Long-type"}}] :loc-begin loc}
-    `("Very bad14!!!" ~loc))
+(defrule exp36-10 @{:type [{:Pointee {:kind "Char-type"}} ... {:Pointee {:kind "LongDouble-type"}}]}
+ "Very bad10!!!")
 
-  (defrule exp3615 @{:type [{:Pointee {:kind "UnsignedChar-type"}} ... {:Pointee {:kind "Float-type"}}] :loc-begin loc}
-    `("Very bad15!!!" ~loc))
+(defrule exp36-11 @{:type [{:Pointee {:kind "UnsignedChar-type"}} ... {:Pointee {:kind "Short-type"}}]}
+ "Very bad11!!!")
 
-  (defrule exp3616 @{:type [{:Pointee {:kind "UnsignedChar-type"}} ... {:Pointee {:kind "UnsignedInt-type"}}] :loc-begin loc}
-    `("Very bad16!!!" ~loc))
+(defrule exp36-12 @{:type [{:Pointee {:kind "UnsignedChar-type"}} ... {:Pointee {:kind "UnsignedShot-type"}}]}
+ "Very bad12!!!")
 
-  (defrule exp3617 @{:type [{:Pointee {:kind "UnsignedChar-type"}} ... {:Pointee {:kind "UnsignedLong-type"}}] :loc-begin loc}
-    `("Very bad17!!!" ~loc))
+(defrule exp36-13 @{:type [{:Pointee {:kind "UnsignedChar-type"}} ... {:Pointee {:kind "Int-type"}}]}
+ "Very bad13!!!")
 
-  (defrule exp3618 @{:type [{:Pointee {:kind "UnsignedChar-type"}} ... {:Pointee {:kind "Double-type"}}] :loc-begin loc}
-    `("Very bad18!!!" ~loc))
+(defrule exp36-14 @{:type [{:Pointee {:kind "UnsignedChar-type"}} ... {:Pointee {:kind "Long-type"}}]}
+ "Very bad14!!!")
 
-  (defrule exp3619 @{:type [{:Pointee {:kind "UnsignedChar-type"}} ... {:Pointee {:kind "LongDouble-type"}}] :loc-begin loc}
-    `("Very bad19!!!" ~loc))
+(defrule exp36-15 @{:type [{:Pointee {:kind "UnsignedChar-type"}} ... {:Pointee {:kind "Float-type"}}]}
+ "Very bad15!!!")
 
-  (defrule exp3620 @{:type [{:Pointee {:kind "Short-type"}} ... {:Pointee {:kind "Int-type"}}] :loc-begin loc}
-    `("Very bad20!!!" ~loc))
+(defrule exp36-16 @{:type [{:Pointee {:kind "UnsignedChar-type"}} ... {:Pointee {:kind "UnsignedInt-type"}}]}
+ "Very bad16!!!")
 
-  (defrule exp3621 @{:type [{:Pointee {:kind "Short-type"}} ... {:Pointee {:kind "Long-type"}}] :loc-begin loc}
-    `("Very bad21!!!" ~loc))
-  
-  (defrule exp3622 @{:type [{:Pointee {:kind "Short-type"}} ... {:Pointee {:kind "Float-type"}}] :loc-begin loc}
-    `("Very bad22!!!" ~loc))
+(defrule exp36-17 @{:type [{:Pointee {:kind "UnsignedChar-type"}} ... {:Pointee {:kind "UnsignedLong-type"}}]}
+ "Very bad17!!!")
 
-  (defrule exp3623 @{:type [{:Pointee {:kind "Short-type"}} ... {:Pointee {:kind "UnsignedInt-type"}}] :loc-begin loc}
-    `("Very bad23!!!" ~loc))
-  
-  (defrule exp3624 @{:type [{:Pointee {:kind "Short-type"}} ... {:Pointee {:kind "UnsignedLong-type"}}] :loc-begin loc}
-    `("Very bad24!!!" ~loc))
+(defrule exp36-18 @{:type [{:Pointee {:kind "UnsignedChar-type"}} ... {:Pointee {:kind "Double-type"}}]}
+ "Very bad18!!!")
 
-  (defrule exp3625 @{:type [{:Pointee {:kind "Short-type"}} ... {:Pointee {:kind "Double-type"}}] :loc-begin loc}
-    `("Very bad25!!!" ~loc))
+(defrule exp36-19 @{:type [{:Pointee {:kind "UnsignedChar-type"}} ... {:Pointee {:kind "LongDouble-type"}}]}
+ "Very bad19!!!")
 
-  (defrule exp3626 @{:type [{:Pointee {:kind "Short-type"}} ... {:Pointee {:kind "LongDouble-type"}}] :loc-begin loc}
-    `("Very bad26!!!" ~loc))
+(defrule exp36-20 @{:type [{:Pointee {:kind "Short-type"}} ... {:Pointee {:kind "Int-type"}}]}
+ "Very bad20!!!")
 
-  (defrule exp3627 @{:type [{:Pointee {:kind "UnsignedShort-type"}} ... {:Pointee {:kind "Int-type"}}] :loc-begin loc}
-    `("Very bad27!!!" ~loc))
+(defrule exp36-21 @{:type [{:Pointee {:kind "Short-type"}} ... {:Pointee {:kind "Long-type"}}]}
+ "Very bad21!!!")
 
-  (defrule exp3628 @{:type [{:Pointee {:kind "UnsignedShort-type"}} ... {:Pointee {:kind "Long-type"}}] :loc-begin loc}
-    `("Very bad28!!!" ~loc))
+(defrule exp36-22 @{:type [{:Pointee {:kind "Short-type"}} ... {:Pointee {:kind "Float-type"}}]}
+ "Very bad22!!!")
 
-  (defrule exp3629 @{:type [{:Pointee {:kind "UnsignedShort-type"}} ... {:Pointee {:kind "Float-type"}}] :loc-begin loc}
-    `("Very bad29!!!" ~loc))
+(defrule exp36-23 @{:type [{:Pointee {:kind "Short-type"}} ... {:Pointee {:kind "UnsignedInt-type"}}]}
+ "Very bad23!!!")
 
-  (defrule exp3630 @{:type [{:Pointee {:kind "UnsignedShort-type"}} ... {:Pointee {:kind "UnsignedInt-type"}}] :loc-begin loc}
-    `("Very bad30!!!" ~loc))
+(defrule exp36-24 @{:type [{:Pointee {:kind "Short-type"}} ... {:Pointee {:kind "UnsignedLong-type"}}]}
+ "Very bad24!!!")
 
-  (defrule exp3631 @{:type [{:Pointee {:kind "UnsignedShort-type"}} ... {:Pointee {:kind "UnsignedLong-type"}}] :loc-begin loc}
-    `("Very bad31!!!" ~loc))
-  
-  (defrule exp3632 @{:type [{:Pointee {:kind "UnsigendShort-type"}} ... {:Pointee {:kind "Double-type"}}] :loc-begin loc}
-    `("Very bad32!!!" ~loc))
+(defrule exp36-25 @{:type [{:Pointee {:kind "Short-type"}} ... {:Pointee {:kind "Double-type"}}]}
+ "Very bad25!!!")
 
-  (defrule exp3633 @{:type [{:Pointee {:kind "UnsignedShort-type"}} ... {:Pointee {:kind "LongDouble-type"}}] :loc-begin loc}
-    `("Very bad33!!!" ~loc))
-  
-  (defrule exp3634 @{:type [{:Pointee {:kind "Int-type"}} ... {:Pointee {:kind "Double-type"}}] :loc-begin loc}
-    `("Very bad34!!!" ~loc))
+(defrule exp36-26 @{:type [{:Pointee {:kind "Short-type"}} ... {:Pointee {:kind "LongDouble-type"}}]}
+ "Very bad26!!!")
 
-  (defrule exp3635 @{:type [{:Pointee {:kind "Int-type"}} ... {:Pointee {:kind "LongDouble-type"}}] :loc-begin loc}
-    `("Very bad35!!!" ~loc))
+(defrule exp36-27 @{:type [{:Pointee {:kind "UnsignedShort-type"}} ... {:Pointee {:kind "Int-type"}}]}
+ "Very bad27!!!")
 
-  (defrule exp3636 @{:type [{:Pointee {:kind "Long-type"}} ... {:Pointee {:kind "Double-type"}}] :loc-begin loc}
-    `("Very bad36!!!" ~loc))
+(defrule exp36-28 @{:type [{:Pointee {:kind "UnsignedShort-type"}} ... {:Pointee {:kind "Long-type"}}]}
+ "Very bad28!!!")
 
-  (defrule exp3637 @{:type [{:Pointee {:kind "Long-type"}} ... {:Pointee {:kind "LongDouble-type"}}] :loc-begin loc}
-    `("Very bad37!!!" ~loc))
+(defrule exp36-29 @{:type [{:Pointee {:kind "UnsignedShort-type"}} ... {:Pointee {:kind "Float-type"}}]}
+ "Very bad29!!!")
 
-  (defrule exp3638 @{:type [{:Pointee {:kind "Float-type"}} ... {:Pointee {:kind "Double-type"}}] :loc-begin loc}
-    `("Very bad38!!!" ~loc))
+(defrule exp36-30 @{:type [{:Pointee {:kind "UnsignedShort-type"}} ... {:Pointee {:kind "UnsignedInt-type"}}]}
+ "Very bad30!!!")
 
-  (defrule exp3639 @{:type [{:Pointee {:kind "Float-type"}} ... {:Pointee {:kind "Double-type"}}] :loc-begin loc}
-    `("Very bad39!!!" ~loc))
+(defrule exp36-31 @{:type [{:Pointee {:kind "UnsignedShort-type"}} ... {:Pointee {:kind "UnsignedLong-type"}}]}
+ "Very bad31!!!")
 
-  (defrule exp3640 @{:type [{:Pointee {:kind "UnsignedInt-type"}} ... {:Pointee {:kind "Double-type"}}] :loc-begin loc}
-    `("Very bad40!!!" ~loc))
+(defrule exp36-32 @{:type [{:Pointee {:kind "UnsigendShort-type"}} ... {:Pointee {:kind "Double-type"}}]}
+ "Very bad32!!!")
 
-  (defrule exp3641 @{:type [{:Pointee {:kind "UnsignedInt-type"}} ... {:Pointee {:kind "LongDouble-type"}}] :loc-begin loc}
-    `("Very bad41!!!" ~loc))
-  
-  (defrule exp3642 @{:type [{:Pointee {:kind "UnsignedLong-type"}} ... {:Pointee {:kind "Double-type"}}] :loc-begin loc}
-    `("Very bad42!!!" ~loc))
+(defrule exp36-33 @{:type [{:Pointee {:kind "UnsignedShort-type"}} ... {:Pointee {:kind "LongDouble-type"}}]}
+ "Very bad33!!!")
 
-  (defrule exp3643 @{:type [{:Pointee {:kind "UnsignedLong-type"}} ... {:Pointee {:kind "LongDouble-type"}}] :loc-begin loc}
-    `("Very bad43!!!" ~loc)))
+(defrule exp36-34 @{:type [{:Pointee {:kind "Int-type"}} ... {:Pointee {:kind "Double-type"}}]}
+ "Very bad34!!!")
+
+(defrule exp36-35 @{:type [{:Pointee {:kind "Int-type"}} ... {:Pointee {:kind "LongDouble-type"}}]}
+ "Very bad35!!!")
+
+(defrule exp36-36 @{:type [{:Pointee {:kind "Long-type"}} ... {:Pointee {:kind "Double-type"}}]}
+ "Very bad36!!!")
+
+(defrule exp36-37 @{:type [{:Pointee {:kind "Long-type"}} ... {:Pointee {:kind "LongDouble-type"}}]}
+ "Very bad37!!!")
+
+(defrule exp36-38 @{:type [{:Pointee {:kind "Float-type"}} ... {:Pointee {:kind "Double-type"}}]}
+ "Very bad38!!!")
+
+(defrule exp36-39 @{:type [{:Pointee {:kind "Float-type"}} ... {:Pointee {:kind "Double-type"}}]}
+ "Very bad39!!!")
+
+(defrule exp36-40 @{:type [{:Pointee {:kind "UnsignedInt-type"}} ... {:Pointee {:kind "Double-type"}}]}
+ "Very bad40!!!")
+
+(defrule exp36-41 @{:type [{:Pointee {:kind "UnsignedInt-type"}} ... {:Pointee {:kind "LongDouble-type"}}]}
+ "Very bad41!!!")
+
+(defrule exp36-42 @{:type [{:Pointee {:kind "UnsignedLong-type"}} ... {:Pointee {:kind "Double-type"}}]}
+ "Very bad42!!!")
+
+(defrule exp36-43 @{:type [{:Pointee {:kind "UnsignedLong-type"}} ... {:Pointee {:kind "LongDouble-type"}}]}
+ "Very bad43!!!")
