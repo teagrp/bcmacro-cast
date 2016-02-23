e68721e58c78cabb7af51eb3654012ce7baeabbb
ayato1995 <ayato.k070206@outlook.jp>
2016-02-24 04:25:46 +0900

    exp40の実装

--- sample/exp40/exp40.clj
+++ sample/exp40/exp40.clj
@@ -1,8 +1,5 @@
-  (defrule exp40a @{:kind "Binop" :op "=" 
-                    :LHS {:type [{:kind "Pointer-type" :Pointee {:kind "Pointer-type" :Pointee {:kind "Char-type" :const "true"}}}]}
-                    :RHS {:HS {:type [{:kind "Pointer-type" :Pointee {:kind "Char-type"}}]}}}
-    "Very badA!!!")
+(defrule exp40-1 @{:type [{:Pointee {}} ... {:Pointee {:Pointee {:const "true"}}}]} 
+  "exp40-1")
   
-  
-  (defrule exp40b @{:type [{:const "true"} ...]}
-  "Very badB!!!")
+(defrule exp40-2 @{:type [ ... {:Pointee {:const "true"}} ... {:Pointee {}}]}
+  "exp40-2")
