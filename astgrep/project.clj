3d7af531900ca9e99c3db430e6a9e9f0c72e0dc4
Seiji Umatani <umatani@gmail.com>
2016-03-30 20:11:09 +0900

    add debug.clj

--- astgrep/project.clj
+++ astgrep/project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojars.umatani/bcmacro "0.2.0-SNAPSHOT"]]
+                 [org.clojars.umatani/bcmacro "0.2.0-SNAPSHOT"]
+                 [instaparse "1.4.1"]]
   :main ^:skip-aot astgrep.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
